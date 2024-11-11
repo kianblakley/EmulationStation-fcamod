9ba6a70d2c55ab6601597fac58f0cdb71d637b1e
Kian Blakley <kian.profiles@gmail.com>
2024-11-11 18:50:23 +1100

    selector image

--- es-app/src/views/SystemView.cpp
+++ es-app/src/views/SystemView.cpp
@@ -37,8 +37,9 @@ SystemView::SystemView(Window* window) : IList<SystemViewData, SystemData*>(wind
 	mExtrasFadeOldCursor = -1;
 	mCarousel.selectedColor = 0;
 	mCarousel.opacityEffect = true;
-	mCarousel.selectorImage = ImageComponent(window);
+	mCarousel.selectorImage = new ImageComponent(window, false, false);
     mCarousel.hasSelectorImage = false;
+    mCarousel.selectedColor = 0;
 
 	setSize((float)Renderer::getScreenWidth(), (float)Renderer::getScreenHeight());
 	populate();
@@ -57,6 +58,12 @@ SystemView::~SystemView()
 		delete mStaticBackground;
 		mStaticBackground = nullptr;
 	}
+
+	if (mCarousel.selectorImage != nullptr)
+    {
+        delete mCarousel.selectorImage;
+        mCarousel.selectorImage = nullptr;
+    }
 
 	clearEntries();
 }
@@ -980,6 +987,26 @@ void SystemView::renderCarousel(const Transform4x4f& trans)
 		bufferRight = 0;
 	}
 
+	// selector image
+	if (mCarousel.hasSelectorImage)
+    {
+        float xOff = 0;
+        float yOff = 0;
+        
+        if (mCarousel.type == HORIZONTAL || mCarousel.type == HORIZONTAL_WHEEL)
+        {
+            xOff = mCamOffset * logoSpacing[0];
+        }
+        else
+        {
+            yOff = mCamOffset * logoSpacing[1];
+        }
+
+        Transform4x4f selectorTrans = carouselTrans;
+        selectorTrans.translate(Vector3f(xOff, yOff, 0));
+        mCarousel.selectorImage->render(selectorTrans);
+    }
+
     for (int i = center - logoCount / 2 + bufferLeft; i <= center + logoCount / 2 + bufferRight; i++)
     {
         int index = i;
@@ -996,26 +1023,6 @@ void SystemView::renderCarousel(const Transform4x4f& trans)
         float scale = 1.0f + ((mCarousel.logoScale - 1.0f) * (1.0f - fabs(distance)));
         scale = Math::min(mCarousel.logoScale, Math::max(1.0f, scale));
         scale /= mCarousel.logoScale;
-
-		// Selector image
-		if (mCarousel.hasSelectorImage) {
-			float xOff = 0;
-			float yOff = 0;
-			
-			// Calculate position based on carousel type
-			if (mCarousel.type == HORIZONTAL || mCarousel.type == HORIZONTAL_WHEEL)
-			{
-				xOff = mCamOffset * logoSpacing[0];
-			}
-			else
-			{
-				yOff = mCamOffset * logoSpacing[1];
-			}
-
-			Transform4x4f selectorTrans = carouselTrans;
-			selectorTrans.translate(Vector3f(xOff, yOff, 0));
-			mCarousel.selectorImage.render(selectorTrans);
-    	}
 
 		// If opacityEffect is true, calculate opacity. Otherwise set to opacity to opaque
         int opacity = mCarousel.opacityEffect ? 
@@ -1398,12 +1405,10 @@ void SystemView::getCarouselFromTheme(const ThemeData::ThemeElement* elem)
 	if (elem->has("selectorImagePath"))
     {
         const std::string& path = elem->get<std::string>("selectorImagePath");
-        mCarousel.selectorImage.setImage(path);
+        mCarousel.selectorImage->setImage(path);
+        mCarousel.selectorImage->setSize(mCarousel.logoSize.x() * mCarousel.logoScale, 
+                                       mCarousel.logoSize.y() * mCarousel.logoScale);
         mCarousel.hasSelectorImage = true;
-        
-        // Set size based on logo size
-        mCarousel.selectorImage.setSize(mCarousel.logoSize.x() * mCarousel.logoScale, 
-                                      mCarousel.logoSize.y() * mCarousel.logoScale);
     }
 }
 
